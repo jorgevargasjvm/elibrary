8450710d46984c50d17553cf773b036db9647341
Javier Artiles <jartiles@simetricaconsulting.com>
2019-12-02 15:23:20 -0400

    FEATURE se terminó de crear el serverjs. Se creo un archivo el cual se encarga de crear y decodificar el token del usuario

--- backend-app/server.js
+++ backend-app/server.js
@@ -1,10 +1,29 @@
-var express = require('express');
-var app = express();
+const config = require('./config');
+// const admin = require('./firebase/fireconfig');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// var db = admin.database();
+var routes = require('./routes');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
+
+app.use('/api', routes);
 
 app.get('/', function (req, res) {
   res.send('API');
 });
-
+// Puerto donde corre la aplicacion localmente.
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000');
 }); 
--- backend-app/services/index.js
+++ backend-app/services/index.js
@@ -1,19 +1,53 @@
-'user strict'
+'use strict'
+// Instancias
+const jwt = require('jwt-simple')
+const moment = require('moment')
+const config = require('../config')
 
-const jwt = require('jwt-simple');
-const moment = require('moment');
-const config = require('../config');
+/**
+ * @name createToken
+ * @description genera el secretToken del usuario
+ * @param {*} user 
+ */
+function createToken (user) {
+  const payload = {
+    sub: user._id,
+    iat: moment().unix(),
+    exp: moment().add(14, 'days').unix()
+  }
 
+  return jwt.encode(payload, config.SECRET_TOKEN)
+}
 
-function createToken(user){
-    const payload = {
-        sub: user._id,
-        iat: moment().unix(),
-        exp: moment().add(7, 'days').unix(),
+/**
+ * @name decodeToken
+ * @description descodifica el token y valida si este aun está activo
+ * @param {*} token 
+ */
+function decodeToken (token) {
+  const decoded = new Promise((resolve, reject) => {
+    try {
+      const payload = jwt.decode(token, config.SECRET_TOKEN)
+
+      if (payload.exp <= moment().unix()) {
+        reject({
+          status: 401,
+          message: 'El token ha expirado'
+        })
+      }
+      resolve(payload.sub)
+    } catch (err) {
+      reject({
+        status: 500,
+        message: 'Invalid Token'
+      })
     }
-    return jwt.encode(payload, config.SECRET_TOKEN);
+  })
+
+  return decoded
 }
 
 module.exports = {
-    createToken
+  createToken,
+  decodeToken
 }
